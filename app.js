02f882c4816d0a8d10e508d4b3b9b5d75f2ba0dc
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-04-30 18:59:23 +0100

    Making progress

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// Import the bot functionalities
+const stickerbot = require('./bots/stickerbot');
+const replymessage = require('./bots/replymessage');
+
+// Initialize the bots
+stickerbot();
+replymessage();
+
